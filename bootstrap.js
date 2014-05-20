44bfd360d4cd60a2c6bf1812a922ca0b4d95ac9a
Henric Englund <hnrcnglnd@gmail.com>
2014-05-20 22:09:06 +0200

    Ignore dotfiles

--- bootstrap.js
+++ bootstrap.js
@@ -3,10 +3,12 @@ module.exports = function(db, path) {
   fs.readdir(path, function(err, files) {
     if (err) throw err;
     files.forEach(function(file) {
-      fs.readFile(path + "/" + file, "utf-8", function(err, doc) {
-        if (err) throw err;
-        db.insert(JSON.parse(doc));
-      });
+      if(file.charAt(0) != ".") {
+        fs.readFile(path + "/" + file, "utf-8", function(err, doc) {
+          if (err) throw err;
+          db.insert(JSON.parse(doc));
+        });
+      }
     });
   });
 }
