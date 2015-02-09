29983a7ebe44c898b2a140cb5874c4291f96794c
Henric Englund <hnrcnglnd@gmail.com>
2015-02-10 00:11:15 +0100

    Set proper feed path yo

--- feed.js
+++ feed.js
@@ -4,7 +4,7 @@ module.exports = function(app, talks, rss) {
     var feed = new rss({
       title: "Aula.io feed",
       description: "The best of tech talks in one place",
-      feed_url: "http://www.aula.io/rss",
+      feed_url: "http://www.aula.io/feed.xml",
       site_url: "http://www.aula.io"
     });
     talks.find({}).sort({ "added": -1 }).exec(function (err, docs) {
